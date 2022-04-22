8b199b5946a4b3e662ba31cc400e3dd5919b9ac0
Ludo <ludovic.barusseau@gmail.com>
2022-04-22 12:39:21 +0200

    API change for getProgressSteps() function

--- infer_covidnet_process.py
+++ infer_covidnet_process.py
@@ -60,7 +60,7 @@ class CovidNetProcess(dataprocess.C2dImageTask):
             for row in f:
                 self.class_names.append(row[:-1])
 
-    def getProgressSteps(self, eltCount=1):
+    def getProgressSteps(self):
         # Function returning the number of progress steps for this process
         # This is handled by the main progress bar of Ikomia application
         return 3
