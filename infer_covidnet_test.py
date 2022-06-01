c0639f7c427362d982471f2befe971a87db1277f
ambroise <ambroise.berthe@ikomia.com>
2022-06-01 16:44:16 +0200

    Fix image loading

--- infer_covidnet_test.py
+++ infer_covidnet_test.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 def test(t, data_dict):
     logger.info("===== Test::infer covidnet =====")
     logger.info("----- Use default parameters")
-    img = cv2.imread(data_dict["images"]["detection"]["coco"], cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(data_dict["images"]["detection"]["coco"])
     input_img_0 = t.getInput(0)
     input_img_0.setImage(img)
     t.run()
